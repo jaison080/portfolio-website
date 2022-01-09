8c76b7a4c4c68115c6d41c005d1ddb889a3b2517
jaison080 <jaisondennis080@gmail.com>
2022-01-09 14:36:19 +0530

    Folders updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,8 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
+
+
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
